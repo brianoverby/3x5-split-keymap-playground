fd5195ad3af727c36b4d44c668a8736f4dface18
brianoverby <mail@brianoverby.dk>
2021-11-15 06:57:45 +0100

    getting started

--- keymap.c
+++ keymap.c
@@ -4,10 +4,10 @@
 // Define keymap layers
 enum keymap_layers {
   _BASE,
-  _LOWER,
-  _RAISE,
-  _FN,
-  _SYSTEM
+  _SYM,
+  _NUM,
+  _FUN,
+  _DK
 };
 
 // Define custom keycodes
@@ -18,9 +18,10 @@ enum keymap_custom_keycodes {
 };
 
 // Define mod keys
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define FN MO(_FN)
+#define SYM MO(_SYM)
+#define NUM MO(_NUM)
+#define FUN MO(_FUN)
+#define ENT_CTL LCTL_T(KC_ENT)
 
 
 // User config 
@@ -46,37 +47,37 @@ bool win_key_enabled = true;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_ortho_4x12(
-        KC_TAB,  DK_Q,    DK_W,    DK_E,    DK_R,    DK_T,    DK_Y,    DK_U,    DK_I,    DK_O,    DK_P,    KC_BSPC,
-        KC_ESC,  DK_A,    DK_S,    DK_D,    DK_F,    DK_G,    DK_H,    DK_J,    DK_K,    DK_L,    DK_SCLN, DK_QUOT,
-        KC_LSFT, DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    DK_N,    DK_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
-        FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   DK_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+        DK_Q,    DK_W,    DK_E,    DK_R,    DK_T,    XXXXXXX, XXXXXXX, DK_Y,    DK_U,    DK_I,    DK_O,    DK_P,
+        DK_A,    DK_S,    DK_D,    DK_F,    DK_G,    XXXXXXX, XXXXXXX, DK_H,    DK_J,    DK_K,    DK_L,    DK_QUOT,
+        DK_Z,    DK_X,    DK_C,    DK_V,    DK_B,    XXXXXXX, XXXXXXX, DK_N,    DK_M,    KC_COMM, KC_DOT,  ENT_CTL,
+        XXXXXXX, XXXXXXX, KC_LGUI, SYM,     KC_LSFT, XXXXXXX, XXXXXXX, KC_SPC,  NUM,     KC_LALT, XXXXXXX, XXXXXXX
     ),
 
-    [_LOWER] = LAYOUT_ortho_4x12(
-        DK_TILD, DK_EXLM, DK_AT,   DK_HASH, DK_DLR,  DK_PERC, DK_CIRC, DK_AMPR, DK_ASTR, DK_LPRN, DK_RPRN, KC_BSPC,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   DK_UNDS, DK_PLUS, DK_LCBR, DK_RCBR, DK_ACUT,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  DK_MINS, DK_EQL,  DK_LBRC, DK_RBRC, _______,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,  KC_SPC,  _______, DK_BSLS, DK_PIPE, XXXXXXX, XXXXXXX
+    [_SYM] = LAYOUT_ortho_4x12(
+        DK_EXLM, DK_AT,   DK_HASH, DK_DLR,  DK_PERC, XXXXXXX, XXXXXXX, DK_TILD, DK_DIAE, DK_LPRN, DK_RPRN, DK_COLN,
+        DK_CIRC, DK_AMPR, DK_UNDS, DK_BSLS, DK_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, DK_QUES, DK_LCBR, DK_RCBR, DK_SCLN,
+        DK_PLUS, DK_MINS, DK_ASTR, DK_SLSH, DK_EQL,  XXXXXXX, XXXXXXX, DK_GRV,  DK_ACUT, DK_LBRC, DK_RBRC, _______,
+        XXXXXXX, XXXXXXX, _______, _______, KC_LCTL, XXXXXXX, XXXXXXX, _______, FUN,     _______, XXXXXXX, XXXXXXX
     ),
 
-    [_RAISE] = LAYOUT_ortho_4x12(
-        DK_GRV,  DK_1,    DK_2,    DK_3,    DK_4,    DK_5,    DK_6,    DK_7,    DK_8,    DK_9,    DK_0,    KC_BSPC,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_DIAE,
-        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, _______, _______, KC_VOLU, _______,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, XXXXXXX, KC_MNXT, KC_VOLD,  KC_MPLY
+    [_NUM] = LAYOUT_ortho_4x12(
+        DK_1,    DK_2,    DK_3,    DK_4,    DK_5,    XXXXXXX, XXXXXXX, KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_BSPC,
+        DK_6,    DK_7,    DK_8,    DK_9,    DK_0,    XXXXXXX, XXXXXXX, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,
+        DK_PLUS, DK_MINS, DK_ASTR, DK_SLSH, DK_EQL,  XXXXXXX, XXXXXXX, KC_ESC,  XXXXXXX, KC_COMM, KC_DOT,  _______,
+        XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX
     ),
 
-    [_FN] = LAYOUT_ortho_4x12(
-        KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  XXXXXXX, KC_PSCR, DK_ARNG,
-        KC_DEL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, DK_AE,   DK_OSTR,
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_DEL,
-        _______, KC_LCTL, KC_LALT, KC_LGUI, _______, _______, _______, _______, XXXXXXX, KC_HOME, KC_PGDN, KC_END
+    [_FUN] = LAYOUT_ortho_4x12(
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, XXXXXXX, RESET,   KC_VOLU, KC_INS,  XXXXXXX, KC_PSCR,
+        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, XXXXXXX, XXXXXXX, XXXXXXX,
+        KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC_TOG, DK_AE,   DK_OSTR, DK_ARNG,
+        XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______, XXXXXXX, XXXXXXX 
     ),
 
-    [_SYSTEM] = LAYOUT_ortho_4x12(
+    [_DK] = LAYOUT_ortho_4x12(
         XXXXXXX, XXXXXXX, WIN_TOG, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_MOD, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_TOG, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC_TOG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MAC_TOG, XXXXXXX, DK_AE,   DK_OSTR, DK_ARNG,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     )
 };
@@ -86,24 +87,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const key_override_t shift_comm_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, DK_LABK);
 const key_override_t shift_dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, DK_RABK);
 const key_override_t shift_quot_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_QUOT, DK_DQUO);
-const key_override_t shift_scln_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_SCLN, DK_COLN); 
-const key_override_t shift_slsh_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_SLSH, DK_QUES); 
+//const key_override_t shift_scln_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_SCLN, DK_COLN); 
+//const key_override_t shift_slsh_key_override = ko_make_basic(MOD_MASK_SHIFT, DK_SLSH, DK_QUES); 
 
 // Key Override array
 const key_override_t **key_overrides = (const key_override_t *[]){
     &shift_comm_key_override,
     &shift_dot_key_override,
     &shift_quot_key_override,
-    &shift_scln_key_override,
-    &shift_slsh_key_override,
+    //&shift_scln_key_override,
+    //&shift_slsh_key_override,
     NULL
 };
 
 
 // SYSTEM layer activation
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _SYSTEM);
-}
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   return update_tri_layer_state(state, _SYM, _NUM, _DK);
+// }
 
 // Process custom keycodes
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -197,7 +198,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       break;
 
-    case DK_RCBR: // {: PC ALGR(KC_0), Mac LSFT(LALT(KC_9))
+    case DK_RCBR: // }: PC ALGR(KC_0), Mac LSFT(LALT(KC_9))
       if(user_config.mac_layout_enabled) {
         if (record->event.pressed) {
           register_code16(LSFT(LALT(KC_9)));
